8ccf2164e0b058995a86ff536365a5080b19bee3
Koros <korosmatick@gmail.com>
2015-12-23 21:33:26 +0300

    Set minimum API level to 7

--- app/src/main/java/com/korosmatick/gridviewexample/MainActivity.java
+++ app/src/main/java/com/korosmatick/gridviewexample/MainActivity.java
@@ -2,6 +2,8 @@ package com.korosmatick.gridviewexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -23,7 +25,7 @@ public class MainActivity extends Activity {
 
     GenericModelAdapter adapter;
     ListView listView;
-    private static final int NUMBER_OF_COLS = 3;
+    private static final int NUMBER_OF_COLS = 4;
     List<Map<String, List<Object>>> items = new ArrayList<Map<String, List<Object>>>();
     Map<String, String> sectionHeaderTitles = new HashMap<String, String>();
 
@@ -93,5 +95,25 @@ public class MainActivity extends Activity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
